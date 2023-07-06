74a720a05447e67ff6b7e2cf7f77c9175018f97a
Jennifer Tan <jtan8485@uni.sydney.edu.au>
2023-07-06 11:02:49 +1000

    Finished unit outline for UoS

--- src/component/UnitOutline.js
+++ src/component/UnitOutline.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Link, Box } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const UnitOutline = () => {
@@ -7,20 +7,34 @@ const UnitOutline = () => {
         <Typography sx={{fontWeight: 700, fontSize: '20px'}}>Unit Outline</Typography>
         <hr className='BottomBorderLine' />
         <Typography sx={{marginBottom: 2}}>Unit outlines will be available 2 weeks before the first day of teaching for the relevant session.</Typography>
-        <Accordion sx={{marginBottom: 2, backgroundColor: '#D9D9D9'}}>
+        <Accordion sx={{marginBottom: 2}}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                 <Typography>Current Year</Typography>
             </AccordionSummary>
             <AccordionDetails>
-                Insert unit outline link here
+                <Box>
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2023-S1C-ND-RE" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 1, 2023 [Normal Day] - Remote</Link>
+                    <Box sx={{ marginBottom: 2 }} />
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2023-S1C-ND-CC" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 1, 2023 [Normal Day] - Camperdown/Darlington, Sydney</Link>
+                </Box>
             </AccordionDetails>
         </Accordion>
-        <Accordion sx={{backgroundColor: '#D9D9D9'}}>
+        <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                 <Typography>Previous Years</Typography>
             </AccordionSummary>
             <AccordionDetails>
-                Insert unit outline links here
+                <Box>
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2020-S1C-ND-CC" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 1, 2020 [Normal Day] - Camperdown/Darlington, Sydney</Link>
+                    <Box sx={{ marginBottom: 2 }} />
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2020-S2C-ND-CC" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 2, 2020 [Normal Day] - Camperdown/Darlington, Sydney</Link>
+                    <Box sx={{ marginBottom: 2 }} />
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2021-S1C-ND-RE" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 1, 2021 [Normal Day] - Remote</Link>
+                    <Box sx={{ marginBottom: 2 }} />
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2022-S1C-ND-CC" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 2, 2022 [Normal Day] - Camperdown/Darlington, Sydney</Link>
+                    <Box sx={{ marginBottom: 2 }} />
+                    <Link href="https://www.sydney.edu.au/units/COMP2017/2022-S1C-ND-RE" underline='none' sx={{color: '#DD432B', fontWeight: 700}}>Semester 1, 2022 [Normal Day] - Remote</Link>
+                </Box>
             </AccordionDetails>
         </Accordion>
         <br />
